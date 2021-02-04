0e997aeea5cdd4c75bcefc3c3de5e274cfa19079
Simon Yawin <simyawin@gmail.com>
2021-02-03 22:40:24 -0500

    define game-repl and custom game-read functions

--- text-adventure.lisp
+++ text-adventure.lisp
@@ -72,3 +72,14 @@
 
 (defun inventory ()
 	(cons 'items- (objects-at 'body *objects* *object-locations*)))
+
+(defun game-repl ()
+	(let ((cmd (game-read)))
+		(unless (eq (car cmd) 'quit)
+			(game-print (game-eval cmd))
+			(game-repl))))
+
+(defun game-read ()
+	(let ((cmd (read-from-string (concatenate 'string "(" (read-line) ")"))))
+		(flet ((quote-it (x) (list 'quote x)))
+			(cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
